123c108e83ddafcfe4b19b3227d5d58769c924d6
Willian Massami Watanabe <watinha@gmail.com>
2025-04-07 19:18:55 -0300

    track event listeners and set the mutation observer api

--- main.py
+++ main.py
@@ -14,6 +14,30 @@ driver.get('http://www.google.com')
 driver.implicitly_wait(10)
 driver.save_screenshot('screenshot.png')
 
+driver.execute_script('''
+
+window.mutations_observed = [];
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        var target = mutation.target;
+        window.mutations_observed.push(target);
+        if (mutation.addedNodes) {
+            for (var i = 0; i < mutation.addedNodes.length; i++) {
+                if (mutation.addedNodes[i].nodeType === 1)
+                    window.mutations_observed.push(mutation.addedNodes[i]);
+            };
+        }
+
+    });
+});
+observer.observe(document.body, {
+    attributes: true,
+    childList: true,
+    subtree: true
+});
+
+''')
+
 root_nodeId = driver.execute_cdp_cmd('DOM.getDocument', {})['root']['nodeId']
 
 all_nodeIds = driver.execute_cdp_cmd('DOM.querySelectorAll', {
@@ -21,18 +45,51 @@ all_nodeIds = driver.execute_cdp_cmd('DOM.querySelectorAll', {
     'selector': 'html > body *'
 })['nodeIds']
 
-nodes_with_listeners = {}
-for nodeId in all_nodeIds:
+event_types = ['blur', 'change', 'click', 'focus', 'input', 'keydown', 'keypress',
+               'keyup', 'mousedown', 'mouseup', 'mouseover', 'mouseout', 'mousemove']
+
+nodes_with_listeners = []
+for i, nodeId in enumerate(all_nodeIds):
     node = driver.execute_cdp_cmd('DOM.describeNode', {'nodeId': nodeId})
     remote_object = driver.execute_cdp_cmd('DOM.resolveNode', { 'nodeId': nodeId })
     events = driver.execute_cdp_cmd('DOMDebugger.getEventListeners',
                                        {'objectId': remote_object['object']['objectId']})
-    if len(events['listeners']) > 0:
-        print(node)
-        print(events)
-        print()
-        print()
 
+    node_events = []
+    for ev in events['listeners']:
+        if ev['type'] in event_types:
+           node_events.append(ev['type'])
+    if len(node_events) > 0:
+        attributes = driver.execute_cdp_cmd('DOM.getAttributes', {'nodeId': nodeId})
+        attributes_obj = {}
+        for i in range(0, len(attributes['attributes']), 2):
+            attributes_obj[attributes['attributes'][i]] = attributes['attributes'][i + 1]
+        className = attributes_obj['class'] if 'class' in attributes_obj else ''
+
+        driver.execute_cdp_cmd('DOM.setAttributeValue', {
+            'nodeId': nodeId,
+            'name': 'class',
+            'value': f'{className} event-listener website-collector-{i}'
+        })
+
+        nodes_with_listeners.append({
+            'node': node,
+            'html': driver.execute_cdp_cmd(
+                'DOM.getOuterHTML', {'nodeId': nodeId})['outerHTML'],
+            'events': node_events
+        })
+
+
+l = driver.execute_script('''	
+
+var list = window.mutations_observed;
+window.mutations_observed = [];
+return list;
+
+''')
+
+print(l)
 
 driver.quit()
+
 
